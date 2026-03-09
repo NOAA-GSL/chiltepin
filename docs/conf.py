88f3731fd120642d0bf5a5e8bc657e532d0b7df7
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2026-03-09 23:21:44 +0000

    Test ReadTheDocs webhook integration

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # Configuration file for the Sphinx documentation builder.
+# Testing ReadTheDocs webhook integration
 
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as get_version
